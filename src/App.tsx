2214f268257596a4f9de748229843e39f2372614
nirkaufman <nirkaufman@gmail.com>
2018-08-07 15:48:28 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React Library</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
